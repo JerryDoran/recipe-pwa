737c84e962d4aa08bd5f736e7ea5d9d8d8225281
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-01-30 14:31:11 -0500

    Finished markup, styles and added manifest file to project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Initialize side nav
+const sideNav = document.querySelector('.side-menu');
+M.Sidenav.init(sideNav, {});
+
+// Initialize side form
+const sideForm = document.querySelector('.side-form');
+M.Sidenav.init(sideForm, {});
