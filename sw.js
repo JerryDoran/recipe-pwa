170b2c1e6a5f2c020e67d93c6dec6d0ef0cfd06c
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-02-04 13:04:52 -0500

    added caching features

--- sw.js
+++ sw.js
@@ -1,14 +1,71 @@
+// Get a reference to the shell resources of the app
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
+
+// Create array of assets that I want cached
+const assets = [
+  '/',
+  '/index.html',
+  '/pages/fallback.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/css/styles.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+];
+
 // Install service worker - self refers to the service worker itself
 self.addEventListener('install', event => {
-  console.log('service worker has been installed');
+  // console.log('service worker has been installed');
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // listen for the activate event of the service worker
 self.addEventListener('activate', event => {
-  console.log('service worker has been activated');
+  // console.log('service worker has been activated');
+
+  // Delete old cache
+  event.waitUntil(
+    caches.keys().then(keys => {
+      // console.log(keys);
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // Fetch event
 self.addEventListener('fetch', event => {
-  console.log('fetch event', event);
+  // console.log('fetch event', event);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(event.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+              cache.put(event.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (event.request.url.indexOf('.html') > -1) {
+          return caches.match('/pages/fallback.html');
+        }
+      })
+  );
 });
